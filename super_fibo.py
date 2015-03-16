8add90f18cb8e1c8dd66fc23a9e01dbc71a1baef
Yaroslav Volohov <yvolohov@gmail.com>
2015-03-16 23:30:02 +0200

    next commit

--- super_fibo.py
+++ super_fibo.py
@@ -1,6 +1,32 @@
 
 def super_fibonacci(n, m):
+
+    if not n > 0:
+        return 0
+
     list = []
 
     for i in range(m):
         list.append(1)
+
+    if n <= m:
+        return list[n - 1]
+
+    start = 0
+    end = m
+    count = n - m
+
+    for i in range(count):
+        number = 0
+
+        for j in range(start, end):
+            number += list[j]
+
+        start += 1
+        end += 1
+        list.append(number)
+
+    return list[n - 1]
+
+
+print(super_fibonacci(9, 3))

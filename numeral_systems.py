87cceb1b054f7e6123c8b86adaf707f50f60e6f6
Yaroslav Volohov <yvolohov@gmail.com>
2015-03-17 14:43:36 +0200

    next commit

--- numeral_systems.py
+++ numeral_systems.py
@@ -1,4 +1,3 @@
-# set python v 2.x for using long
 
 def convert_n_to_m(x, n, m):
     import math
@@ -23,20 +22,20 @@ def convert_n_to_m(x, n, m):
             return False
 
     # from n to decimal system
-    number10 = 0L
+    number10 = 0
 
     if n > 1:
         for i in range(length):
             digit = x[length - i - 1]
             digit10 = sys_symbols.find(digit)
-            number10 += (digit10 * long(math.pow(n, i)))
+            number10 += (digit10 * (n**i)) # do not use pow, use **
     else:
         number10 = len(x)
 
     # from decimal system to m
     if m > 1:
-        r = long(number10 % m)
-        q = long(number10 // m)
+        r = number10 % m
+        q = number10 // m
         result = symbols[r]
 
         while q > 0:

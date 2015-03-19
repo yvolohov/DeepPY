38e379b5cba67d549a52ba0d482042ab5cf9045c
Yaroslav Volohov <yvolohov@gmail.com>
2015-03-19 23:33:13 +0200

    next commit

--- saddle_point.py
+++ saddle_point.py
@@ -1,8 +1,19 @@
 
 def saddle_point(matrix):
+
+    def get_col(index):
+        col = []
+        for row in matrix:
+            col.append(row[index])
+        return col
+
+    rows_count = len(matrix)
+
+    if rows_count == 0:
+        return False
+
     col_elements = [] # max
     row_elements = [] # min
-    rows_count = len(matrix)
 
     for row_index in range(rows_count):
         min_elem = min(matrix[row_index])
@@ -10,9 +21,24 @@ def saddle_point(matrix):
 
         if count_elems == 1:
             index_elem = matrix[row_index].index(min_elem)
-            row_elements.append((row_index, index_elem, min_elem))
+            row_elements.append((row_index, index_elem))
 
-    print(row_elements)
+    cols_count = len(matrix[0])
 
-saddle_point([[8,3,0,1,2,3,4,8,1,2,3],[3,2,1,2,3,9,4,7,9,2,3],[7,6,0,1,3,5,2,3,4,1,1]])
+    for col_index in range(cols_count):
+        column = get_col(col_index)
+        max_elem = max(column)
+        count_elems = column.count(max_elem)
+
+        if count_elems == 1:
+            index_elem = column.index(max_elem)
+            col_elements.append((index_elem, col_index))
+
+    for point in col_elements:
+        if point in row_elements:
+            return point
+
+    return False
+
+print(saddle_point([[21]]))
 
